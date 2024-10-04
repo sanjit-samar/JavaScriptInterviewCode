c149b9507c78f14cf8c604b1adccaad707f543a3
sanjit samar <sanjit1525@gmail.com>
2024-10-04 09:29:49 +0530

    reverse array problem

---
+++ Arrays/reverseArray.js
@@ -0,0 +1,13 @@
+//by help of for loop
+let arr =[1,2,3,4,5,6];
+
+function reverse(arr){
+    let uniqueArr =[];
+    for(let i = arr.length-1; i >=0; i--){
+        uniqueArr.push(arr[i]);
+    }
+    return uniqueArr;
+}
+
+const result = reverse(arr);
+console.log(result);
